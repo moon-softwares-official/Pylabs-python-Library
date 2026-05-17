0afa3f3c10fe00d122129287020e0705dca81adf
moon-softwares-official <gloomydarkness1234@gmail.com>
2026-05-17 19:21:26 -0300

    Add setup.py for package configuration

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pylabs",
+    version="1.0.0",
+    author="Moon Softwares",
+    packages=find_packages(),
+    install_requires=["requests"]
+)
